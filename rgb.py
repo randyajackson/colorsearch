a70192473874989f5aae756efef42d084978ca84
randyajackson <rajackson.dev@gmail.com>
2019-02-26 21:43:38 -0700

    reducing rgb values to int values for input into sql

--- rgb.py
+++ rgb.py
@@ -1,4 +1,20 @@
 from PIL import Image
 im = Image.open("0.jpg")
 pix = list(im.getdata())
-print(pix)
+
+pd = {}
+count = 1
+temp = 0
+
+for x in pix:
+    rgb = str(x)
+    rgb = rgb.replace(' ', '').replace('(', '').replace(')', '').replace(',', '')
+
+    if(rgb not in pd):
+        pd[rgb] = count
+    else:
+        temp = pd[rgb]
+        temp += 1
+        pd[rgb] = temp
+
+print(pd)
