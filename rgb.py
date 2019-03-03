1a3240f8b7a5b9b670a983c4c11b0f0e0391c41a
randyajackson <rajackson.dev@gmail.com>
2019-03-03 10:53:26 -0700

    Truncated pictures to 10, inserting rgb data into mongodb

--- rgb.py
+++ rgb.py
@@ -1,28 +1,36 @@
 from PIL import Image
 import pandas as pd
 import numpy as np
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client['art-data']
+pictures = db.art_full_info
+
+print(pictures)
 
 Image.MAX_IMAGE_PIXELS = None
 
 data = pd.read_csv("data_condensed.csv")
 
-im = Image.open("img/" + data['new_filename'][0], 'r')
+for z in range(10):
+    im = Image.open("img/" + data['new_filename'][z], 'r')
+    print("in " + str(z))
+    fullDict = {}
 
-entry = {}
+    initial = 1
+    id = 0
 
-initial = 1
-id = 0
+    for x in iter(im.getdata()):
+        if(x not in fullDict):
+            fullDict[x] = initial
+        else:
+            temp = fullDict[x]
+            temp += 1
+            fullDict[x] = temp
 
-for x in iter(im.getdata()):
-    if(x not in entry):
-        entry[x] = initial
-    else:
-        temp = entry[x]
-        temp += 1
-        entry[x] = temp
-
-print(entry)
-print(data['genre'][0])
-print(data['style'][0])
-print(data['title'][0])
-print("img/" + data['new_filename'][0])
+    fullDict['genre'] = data['genre'][z]
+    fullDict['style'] = data['style'][z]
+    fullDict['title'] = data['title'][z]
+    fullDict['fileName'] ="img/" + data['new_filename'][z]
+    print(fullDict)
